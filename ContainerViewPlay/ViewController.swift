bdf68eb9584c486738dabbf781ab37bda7ff40f9
beepscore <support@beepscore.com>
2018-04-26 21:53:37 -0700

    Add IBOutlet aContainerViewHeight and set it

--- ContainerViewPlay/ViewController.swift
+++ ContainerViewPlay/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var aContainerView: UIView!    
     @IBOutlet weak var bContainerView: UIView!
+
+    @IBOutlet weak var aContainerViewHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,19 +31,9 @@ class ViewController: UIViewController {
     /// https://stackoverflow.com/questions/26180822/swift-adding-constraints-programmatically#26181982
     /// https://stackoverflow.com/questions/30421206/how-to-set-size-uiview-in-auto-layout-programmatically-swift
     func configureAContainerView() {
-
         aContainerView.translatesAutoresizingMaskIntoConstraints = false
-
-        // let horizontalConstraint = aContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        // let verticalConstraint = NSLayoutConstraint(item: aContainerView, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.top, multiplier: 1, constant: 40)
-
-        // let widthConstraint = aContainerView.widthAnchor.constraint(equalTo: view.widthAnchor)
-        let heightConstraint = aContainerView.heightAnchor.constraint(equalToConstant: 12)
-        // view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
-        // NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
-        NSLayoutConstraint.activate([heightConstraint])
+        aContainerViewHeight.constant = 12
     }
-
 
 }
 
